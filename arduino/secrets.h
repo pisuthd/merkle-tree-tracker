79a9055c4ec0951e54868e5d2e8d544f680345b7
Pisuth D <pisuth.dae@gmail.com>
2020-06-24 23:06:55 +0700

    Feat: Add Esp8266's .ino files

---
+++ arduino/secrets.h
@@ -0,0 +1,9 @@
+#include <pgmspace.h>
+
+#define SECRET
+#define THINGNAME ""
+
+const char WIFI_SSID[] = "";
+const char WIFI_PASSWORD[] = "";
+const char AWS_DOMAIN[] = "";
+const char AWS_IOT_PUBLISH_TOPIC[] = "";
