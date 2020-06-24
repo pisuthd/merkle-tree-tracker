bf0619aeea6122bcebbfdd63d0f8dacc6eac4763
Pisuth D <pisuth.dae@gmail.com>
2020-06-24 23:04:56 +0700

    Fix: Remove static host in the script as going to setup manually

--- index.js
+++ index.js
@@ -6,46 +6,6 @@ const mime = require("mime");
 const { generateProof, capture, getStats, search } = require("./lambda-functions/merkleTree");
 
 
-const siteBucket = new aws.s3.Bucket("merkle-tree-tracker-web", {
-    website: {
-        indexDocument: "index.html",
-    },
-});
-
-const siteDir = "www";
-
-for (let item of require("fs").readdirSync(siteDir)) {
-    let filePath = require("path").join(siteDir, item);
-    let object = new aws.s3.BucketObject(item, {
-        bucket: siteBucket,
-        source: new pulumi.asset.FileAsset(filePath),     // use FileAsset to point to a file
-        contentType: mime.getType(filePath) || undefined, // set the MIME type of the file
-    });
-}
-
-function publicReadPolicyForBucket(bucketName) {
-    return JSON.stringify({
-        Version: "2012-10-17",
-        Statement: [{
-            Effect: "Allow",
-            Principal: "*",
-            Action: [
-                "s3:GetObject"
-            ],
-            Resource: [
-                `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
-            ]
-        }]
-    })
-}
-
-// Set the access policy for the bucket so all objects are readable
-const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
-    bucket: siteBucket.bucket, // depends on siteBucket -- see explanation below
-    policy: siteBucket.bucket.apply(publicReadPolicyForBucket)
-    // transform the siteBucket.bucket output property -- see explanation below
-});
-
 
 // Create tables
 const dataTable = new aws.dynamodb.Table(
@@ -94,8 +54,4 @@ const endpoint = new awsx.apigateway.API("merkle-tree-tracker-api", {
     ],
 })
 
-
-
-exports.bucketName = siteBucket.bucket;
-exports.websiteUrl = siteBucket.websiteEndpoint;
 exports.apiEndpoint = endpoint.url;
